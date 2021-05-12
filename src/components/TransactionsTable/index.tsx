ddd1ccc0da0730966d878b03b6f170b462c650e8
edrajao <edrajao@iskola.com.br>
2021-05-12 09:48:20 -0300

    First Commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,45 @@
+import { Container } from './styles';
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de WEB</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>25/04/2021</td>
+          </tr>
+          <tr>
+            <td>Médico</td>
+            <td className="withdraw">-R$ 1.000,00</td>
+            <td>Cuidados com a Saúde</td>
+            <td>05/05/2021</td>
+          </tr>
+          <tr>
+            <td>Internet JubaLiba</td>
+            <td className="withdraw">-R$ 120,00</td>
+            <td>Infra Estrutura</td>
+            <td>26/05/2021</td>
+          </tr>
+          <tr>
+            <td>Placa Mãe</td>
+            <td className="withdraw">-R$ 660,00</td>
+            <td>Manutenção de Equipamentos</td>
+            <td>27/04/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
