51baca86e3e6843c8e25c48b9b42d1fa3d45a47d
edrajao <edrajao@iskola.com.br>
2021-05-12 16:08:40 -0300

    Second Commit

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,21 @@
+import Modal from 'react-modal';
+
+import { Container } from './styles';
+
+interface NewTransactionProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionProps) {
+  return (
+    <Container>
+      <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+        <h2>Cadastrar Transação</h2>
+      </Modal>
+    </Container>
+  );
+}
